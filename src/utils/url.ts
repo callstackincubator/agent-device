e42baaca654568d44876ee906989a419f94eb9c3
Michał Pierzchała <thymikee@gmail.com>
2026-04-10 12:59:25 +0200

    refactor: avoid regex in base url normalization

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,7 @@
 export function normalizeBaseUrl(input: string): string {
-  return input.replace(/\/+$/, '');
+  let end = input.length;
+  while (end > 0 && input.charCodeAt(end - 1) === 47) {
+    end -= 1;
+  }
+  return end === input.length ? input : input.slice(0, end);
 }
